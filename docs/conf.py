0dd02862579b0a681fd571f7efd2cd8cb5d57d29
Celine Durniak <celine.durniak@esss.se>
2020-07-01 20:02:29 +0200

    Removed sphinx-gallery from conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ extensions = [
    # 'sphinx.ext.napoleon',
     'nbsphinx',
     'sphinx.ext.mathjax',
-    'sphinx_gallery.load_style',
+  #  'sphinx_gallery.load_style',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
