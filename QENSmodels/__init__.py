dd7110a267de16cc4a6294b4e84f02bd88ff5363
Celine <celine.durniak@esss.se>
2022-05-29 14:24:32 +0200

    Add version to __init__

--- QENSmodels/__init__.py
+++ QENSmodels/__init__.py
@@ -1,5 +1,28 @@
 # -*- coding: utf-8 -*-
 
+from ._version import __version__
+
+from .lorentzian import lorentzian
+from .brownian_translational_diffusion import hwhmBrownianTranslationalDiffusion
+from .brownian_translational_diffusion import sqwBrownianTranslationalDiffusion
+from .delta import delta
+from .delta_lorentz import sqwDeltaLorentz
+from .gaussian import gaussian
+from .gaussian_model_3d import hwhmGaussianModel3D
+from .gaussian_model_3d import sqwGaussianModel3D
+from .delta_two_lorentz import sqwDeltaTwoLorentz
+from .isotropic_rotational_diffusion import sqwIsotropicRotationalDiffusion
+from .isotropic_rotational_diffusion import hwhmIsotropicRotationalDiffusion
+from .jump_sites_log_norm_dist import hwhmJumpSitesLogNormDist
+from .jump_sites_log_norm_dist import sqwJumpSitesLogNormDist
+from .jump_translational_diffusion import hwhmJumpTranslationalDiffusion
+from .jump_translational_diffusion import sqwJumpTranslationalDiffusion
+from .water_teixeira import sqwWaterTeixeira
+from .background_polynomials import background_polynomials
+from .chudley_elliott_diffusion import hwhmChudleyElliottDiffusion
+from .chudley_elliott_diffusion import sqwChudleyElliottDiffusion
+from .equivalent_sites_circle import hwhmEquivalentSitesCircle
+from .equivalent_sites_circle import sqwEquivalentSitesCircle
 from .lorentzian import lorentzian  # noqa
 from .brownian_translational_diffusion import hwhmBrownianTranslationalDiffusion  # noqa
 from .brownian_translational_diffusion import sqwBrownianTranslationalDiffusion  # noqa
