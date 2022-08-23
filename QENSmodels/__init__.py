213213d085c51876a2ffa7c29ea1b2e12499c5c6
celinedurniak <celinedurniak@users.noreply.github.com>
2022-08-23 16:17:47 +0200

    Update __init__.py

--- QENSmodels/__init__.py
+++ QENSmodels/__init__.py
@@ -1,50 +1,5 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa : F401
-from ._version import __version__
-
-from .lorentzian import lorentzian
-from .brownian_translational_diffusion import hwhmBrownianTranslationalDiffusion
-from .brownian_translational_diffusion import sqwBrownianTranslationalDiffusion
-from .delta import delta
-from .delta_lorentz import sqwDeltaLorentz
-from .gaussian import gaussian
-from .gaussian_model_3d import hwhmGaussianModel3D
-from .gaussian_model_3d import sqwGaussianModel3D
-from .delta_two_lorentz import sqwDeltaTwoLorentz
-from .isotropic_rotational_diffusion import sqwIsotropicRotationalDiffusion
-from .isotropic_rotational_diffusion import hwhmIsotropicRotationalDiffusion
-from .jump_sites_log_norm_dist import hwhmJumpSitesLogNormDist
-from .jump_sites_log_norm_dist import sqwJumpSitesLogNormDist
-from .jump_translational_diffusion import hwhmJumpTranslationalDiffusion
-from .jump_translational_diffusion import sqwJumpTranslationalDiffusion
-from .water_teixeira import sqwWaterTeixeira
-from .background_polynomials import background_polynomials
-from .chudley_elliott_diffusion import hwhmChudleyElliottDiffusion
-from .chudley_elliott_diffusion import sqwChudleyElliottDiffusion
-from .equivalent_sites_circle import hwhmEquivalentSitesCircle
-from .equivalent_sites_circle import sqwEquivalentSitesCircle
-from .lorentzian import lorentzian  # noqa
-from .brownian_translational_diffusion import hwhmBrownianTranslationalDiffusion  # noqa
-from .brownian_translational_diffusion import sqwBrownianTranslationalDiffusion  # noqa
-from .delta import delta  # noqa
-from .delta_lorentz import sqwDeltaLorentz  # noqa
-from .gaussian import gaussian  # noqa
-from .gaussian_model_3d import hwhmGaussianModel3D  # noqa
-from .gaussian_model_3d import sqwGaussianModel3D  # noqa
-from .delta_two_lorentz import sqwDeltaTwoLorentz  # noqa
-from .isotropic_rotational_diffusion import sqwIsotropicRotationalDiffusion  # noqa
-from .isotropic_rotational_diffusion import hwhmIsotropicRotationalDiffusion  # noqa
-from .jump_sites_log_norm_dist import hwhmJumpSitesLogNormDist  # noqa
-from .jump_sites_log_norm_dist import sqwJumpSitesLogNormDist  # noqa
-from .jump_translational_diffusion import hwhmJumpTranslationalDiffusion  # noqa
-from .jump_translational_diffusion import sqwJumpTranslationalDiffusion  # noqa
-from .water_teixeira import sqwWaterTeixeira  # noqa
-from .background_polynomials import background_polynomials  # noqa
-from .chudley_elliott_diffusion import hwhmChudleyElliottDiffusion  # noqa
-from .chudley_elliott_diffusion import sqwChudleyElliottDiffusion  # noqa
-from .equivalent_sites_circle import hwhmEquivalentSitesCircle  # noqa
-from .equivalent_sites_circle import sqwEquivalentSitesCircle  # noqa
-#
 # __all__ = ['background_polynomials',
 #            'lorentzian',
 #            'hwhmBrownianTranslationalDiffusion',
